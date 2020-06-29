29366fca955731651ccca054a0ab4f668ae22a98
Natthapong Intharak <nattha.int@gmail.com>
2020-06-29 23:39:45 +0700

    Normal Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Modal } from "antd";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
+import "antd/dist/antd.css"
 
 function App() {
+  const { confirm } = Modal;
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const showModal = () => {
+    setModalVisible(true);
+  };
+
+  const showModalWithIcon = () => {
+    confirm({
+      title: "Do you want to approve this activity and all its session(s)?",
+      icon: <ExclamationCircleOutlined/>,
+      content: "Please make sure that you have read through all the details as the sessions will be live upon approved.",
+      onOk() {
+        console.log("OK")
+      },
+      onCancel() {
+        console.log("Cancel")
+      }
+    })
+  }
+
+  const onModalOk = () => {
+    console.log("onModalOk");
+    setModalVisible(false);
+  };
+
+  const onModalCancel = () => {
+    console.log("onModalCancel");
+    setModalVisible(false);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +52,18 @@ function App() {
         >
           Learn React
         </a>
+
+        <Button type="primary" onClick={ () => showModal() }>Approve this activity</Button>
+        <Button type="primary" onClick={ () => showModalWithIcon() }>Another Approve Activity</Button>
       </header>
+      <Modal
+        title="This is a basic modal"
+        visible={ modalVisible }
+        onOk={ () => onModalOk() }
+        onCancel={ () => onModalCancel() }
+        >
+        Please make sure
+      </Modal>
     </div>
   );
 }
