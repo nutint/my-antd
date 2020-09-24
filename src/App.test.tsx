f6856477c9ce9ec1795e0b4f5a7f60532c5ca4b8
Natthapong Intharak <natthapong.intharak@thoughtworks.com>
2020-09-24 22:10:49 +0700

    Prepare for eject

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-  expect(getByText("Approve this activity")).toBeInTheDocument();
+describe("App", () => {
+  test('renders learn react link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+    expect(getByText("Approve this activity")).toBeInTheDocument();
+  });
 });
