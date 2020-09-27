775964bee024070f7379eaaa61a05c0eab81c3d9
Natthapong Intharak <natthapong.intharak@thoughtworks.com>
2020-09-27 16:40:17 +0700

    Successfully integrate router

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import "./App.css"
 import { Layout, Menu, Breadcrumb } from "antd"
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from "@ant-design/icons"
 import "antd/dist/antd.css"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+
 
 const { Header, Sider, Content } = Layout
 const { SubMenu } = Menu
@@ -10,57 +17,72 @@ const { SubMenu } = Menu
 const App = () => {
   return (
     <div className="App">
-      <Layout>
-        <Header className="header">
-          <div className="logo" />
-        </Header>
+      <Router>
         <Layout>
-          <Sider width={200} className="site-layout-background">
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={["1"]}
-              defaultOpenKeys={["sub1"]}
-              style={{ height: "100%", borderRight: 0 }}
-            >
-              <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                {
-                  Array.from({ length: 4 }, (_, i) => i + 1)
-                    .map(elem => (<Menu.Item key={elem}>Option {elem}</Menu.Item>))
-                }
-              </SubMenu>
-              <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                <Menu.Item key="5">option5</Menu.Item>
-                <Menu.Item key="6">option6</Menu.Item>
-                <Menu.Item key="7">option7</Menu.Item>
-                <Menu.Item key="8">option8</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-                <Menu.Item key="9">option9</Menu.Item>
-                <Menu.Item key="10">option10</Menu.Item>
-                <Menu.Item key="11">option11</Menu.Item>
-                <Menu.Item key="12">option12</Menu.Item>
-              </SubMenu>
-            </Menu>
-          </Sider>
-          <Layout style={{ padding: "0 24px 24px" }}>
-            <Breadcrumb style={{ margin: "16px 0" }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
-            <Content
-              className="site-layout-background"
-              style={{
-                padding: 24,
-                margin: 0,
-                minHeight: 280,
-              }}
-            >
-              Content
-            </Content>
+          <Header className="header">
+            <div className="logo" />
+          </Header>
+          <Layout>
+            <Sider width={200} className="site-layout-background">
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={["1"]}
+                defaultOpenKeys={["sub1"]}
+                style={{ height: "100%", borderRight: 0 }}
+              >
+                <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+                  {
+                    Array.from({ length: 4 }, (_, i) => i + 1)
+                      .map(elem => (<Menu.Item key={elem}>Option {elem}</Menu.Item>))
+                  }
+                </SubMenu>
+                <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+                  <Menu.Item key="5">
+                    <Link to="/about">About</Link>
+                  </Menu.Item>
+                  <Menu.Item key="6">
+                    <Link to="/other">Other</Link>
+                  </Menu.Item>
+                  <Menu.Item key="7">option7</Menu.Item>
+                  <Menu.Item key="8">option8</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+                  <Menu.Item key="9">option9</Menu.Item>
+                  <Menu.Item key="10">option10</Menu.Item>
+                  <Menu.Item key="11">option11</Menu.Item>
+                  <Menu.Item key="12">option12</Menu.Item>
+                </SubMenu>
+              </Menu>
+            </Sider>
+            <Layout style={{ padding: "0 24px 24px" }}>
+              <Breadcrumb style={{ margin: "16px 0" }}>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>List</Breadcrumb.Item>
+                <Breadcrumb.Item>App</Breadcrumb.Item>
+              </Breadcrumb>
+              <Content
+                className="site-layout-background"
+                style={{
+                  padding: 24,
+                  margin: 0,
+                  minHeight: 280,
+                }}
+              >
+
+                <Switch>
+                  <Route path="/about">
+                    About
+                  </Route>
+                  <Route path="/other">
+                    Other
+                  </Route>
+                </Switch>
+
+              </Content>
+            </Layout>
           </Layout>
         </Layout>
-      </Layout>
+      </Router>
     </div>
   )
 }
